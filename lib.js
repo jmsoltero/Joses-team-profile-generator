afe790ca4d636d15b775dfdb0ae4eab0f2b442c1
josemanuelsoltero <josemanuelsoltero@joses-MacBook-Pro.local>
2023-02-03 17:00:58 -0600

    adds employee classes

---
+++ lib.js
@@ -0,0 +1,25 @@
+class Employee {
+    constructor(name,id,email) {
+        this.name = name,
+        this.id = id,
+        this.email = email
+    } getName(){
+
+    } getEmail(){
+
+    } getRole(){
+
+    }
+}
+
+class Manager extends Employee {
+
+}
+
+class Enginner extends Employee{
+
+}
+
+class Intern extends Employee{
+    
+}
